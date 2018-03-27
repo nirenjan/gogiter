34b45d7d1883bc26e99426a38cdea65840f46c78
nirenjan <nirenjan@gmail.com>
2018-03-27 01:16:23 -0700

    Make dump_backtrace a shared function

--- lib/core.bash
+++ lib/core.bash
@@ -18,7 +18,7 @@ warn()
 colorize_stackframe()
 {
     local frame=$1
-    local output=$(caller $frame) 
+    local output=$(caller $frame)
 
     if [[ -z "$output" ]]
     then
@@ -30,10 +30,23 @@ colorize_stackframe()
     local lineno=$1
     local func=$2
     local file=${3#$GGROOT/}
-    
+
     echo -ne "\t$(tput setaf 2)$file$(tput sgr0):$(tput setaf 3)$lineno "
     echo -e "\t$(tput sgr0)$func"
     return 0
+}
+
+# Dump backtrace
+# This is used to dump the complete call stack
+dump_backtrace()
+{
+    # Start frame from 1, since we are calling a new function for
+    # colorizing the stackframe
+    local frame=${1:-1}
+    while colorize_stackframe $frame
+    do
+        ((frame++))
+    done
 }
 
 # Panic function - aborts and exits with an error
@@ -46,13 +59,7 @@ panic()
     # Dump stacktrace
     if [[ -n "$GGDEBUG" ]]
     then
-        # Start frame from 1, since we are calling a new function for
-        # colorizing the stackframe
-        local frame=1
-        while colorize_stackframe $frame
-        do
-            ((frame++))
-        done
+        dump_backtrace
     fi
 
     exit $exit_code
@@ -66,3 +73,20 @@ list_contains()
     [[ "$2" == "$1" || "$2" == "$1:"* || "$2" == *":$1" || "$2" == *":$1:"* ]]
 }
 
+# Exit handler
+exit_handler()
+{
+    local exit_code=$?
+
+    # If the script exits with a failure, then dump the backtrace
+    if [[ $exit_code != 0 ]]
+    then
+        # Obviously, we don't want the exit_handler showing up in
+        # the backtrace - skip it
+        [[ -n "${GGBACKTRACE:-}" ]] && dump_backtrace 2
+    fi
+
+    return $?
+}
+
+trap exit_handler EXIT
